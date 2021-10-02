243225a22b435e7abe042adf210839b805f4c7db
Chris Wilding <hello@chriswilding.co.uk>
2021-10-02 17:40:04 +0100

    Add some Heroku specific config for Flyway (#6)

--- api/src/main/java/uk/co/chriswilding/magidash/config/DatabaseConfig.java
+++ api/src/main/java/uk/co/chriswilding/magidash/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package uk.co.chriswilding.magidash.config;
 
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,20 +10,32 @@ import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 @Configuration
 @Profile("heroku")
 public class DatabaseConfig {
 
-    @Bean
-    private static Connection getConnection() throws URISyntaxException, SQLException {
+    private static List<String> getJdbcUrlUsernameAndPassword() throws URISyntaxException {
         URI databaseUri = new URI(System.getenv("JAWSDB_URL"));
 
         String username = databaseUri.getUserInfo().split(":")[0];
         String password = databaseUri.getUserInfo().split(":")[1];
         String port = String.valueOf(databaseUri.getPort());
-        String jdbUrl = "jdbc:mysql://" + databaseUri.getHost() + ":" + port + databaseUri.getPath();
+        String jdbcUrl = "jdbc:mysql://" + databaseUri.getHost() + ":" + port + databaseUri.getPath();
 
-        return DriverManager.getConnection(jdbUrl, username, password);
+        return List.of(jdbcUrl, username, password);
+    }
+
+    @Bean
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        var properties = getJdbcUrlUsernameAndPassword();
+        return DriverManager.getConnection(properties.get(0), properties.get(1), properties.get(2));
+    }
+
+    @Bean
+    private static FlywayConfigurationCustomizer getFlywayConfigurationCustomizer() throws URISyntaxException {
+        var properties = getJdbcUrlUsernameAndPassword();
+        return configuration -> configuration.dataSource(properties.get(0), properties.get(1), properties.get(2));
     }
 }

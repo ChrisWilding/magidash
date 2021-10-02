11f54c26b43b10e65663a65e39f99a962d55fdc6
Chris Wilding <hello@chriswilding.co.uk>
2021-10-02 14:32:35 +0100

    Initialise MagiDash
    * Initialise MagiDash with Spring Initializr
    * Move database script into migrations
    * Add just enough config for api to boot

---
+++ api/src/main/java/uk/co/chriswilding/magidash/MagidashApplication.java
@@ -0,0 +1,13 @@
+package uk.co.chriswilding.magidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagidashApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MagidashApplication.class, args);
+  }
+
+}
---
+++ api/src/test/java/uk/co/chriswilding/magidash/MagidashApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.chriswilding.magidash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagidashApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

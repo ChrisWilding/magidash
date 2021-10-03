d35888abc8bd19a381b0327fdd20a7687444bdcf
Chris Wilding <hello@chriswilding.co.uk>
2021-10-03 16:09:22 +0100

    Add dataloading wrapper for DashboardTable component

--- web/pages/dashboards/index.tsx
+++ web/pages/dashboards/index.tsx
@@ -1,22 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import useSWR from 'swr'
-import styles from '../../styles/Home.module.css'
-import Footer from '../../components/Footer'
 
-import fetcher from '../../services/fetcher'
-import DashboardTable from '../../components/DashboardTable'
 import Container from '../../components/Container'
+import DashboardTable from '../../components/DashboardTable'
 
 const Dashboards: NextPage = () => {
-  const { data, error } = useSWR('/api/dashboards', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-
   return (
     <Container title="MagiDash Dashboards" description="A table of MagiDash dashboards">
-      <DashboardTable dashboards={data} />
+      <DashboardTable />
     </Container>
   )
 }

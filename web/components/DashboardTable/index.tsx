c2e33e682b20da42dc2a33d742168c09e1b17d4b
Chris Wilding <hello@chriswilding.co.uk>
2021-10-03 16:50:34 +0100

    Add tests for DashboardTableContainer

--- web/components/DashboardTable/index.tsx
+++ web/components/DashboardTable/index.tsx
@@ -9,7 +9,7 @@ const DashboardTableContainer: FunctionComponent = () => {
   const { data, error } = useSWR('/api/dashboards', fetcher)
 
   if (error) return <div>Sorry, something went wrong retrieving the dashboards</div>
-  if (!data) return <div>loading some beautiful dashboards, hang tight ...</div>
+  if (!data) return <div>Loading some beautiful dashboards, hang tight ...</div>
 
   return <DashboardTable dashboards={data} />
 }

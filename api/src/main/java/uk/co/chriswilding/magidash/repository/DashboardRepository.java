6371326aa77e26521148e6303b97458ddecb829c
Chris Wilding <hello@chriswilding.co.uk>
2021-10-06 11:34:14 +0100

    Get dashboard by id (#26)

--- api/src/main/java/uk/co/chriswilding/magidash/repository/DashboardRepository.java
+++ api/src/main/java/uk/co/chriswilding/magidash/repository/DashboardRepository.java
@@ -3,7 +3,9 @@ package uk.co.chriswilding.magidash.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DashboardRepository extends JpaRepository<DashboardEntity, Long> {
     List<DashboardEntity> findAll();
+    Optional<DashboardEntity> findOneById(Long id);
 }
